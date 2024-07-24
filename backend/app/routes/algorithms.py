91ca6d8d9d76ee9b9131e3124ddcbbf365269da1
David Silva <daverhan02@gmail.com>
2024-07-24 00:10:06 -0400

    Create scaffold for Bernstein-Vazirani algorithm

--- backend/app/routes/algorithms.py
+++ backend/app/routes/algorithms.py
@@ -40,15 +40,14 @@ def simons():
 def bernstein_vazirani():
     request_json = request.get_json()
 
-    secret_bitstring = request_json.get('secret_bitstring')
+    secret_bitstring = request_json.get('bitstring')
 
     if not secret_bitstring:
-        return jsonify({'error': 'The secret bitstring was not found in the request'}), 400
+        return jsonify({'error': 'The bitstring was not found in the request'}), 400
 
     secret_bitstring, quantum_circuit = execute_bernstein_vazirani(
         secret_bitstring)
 
-    print(f'The secret bitstring is {secret_bitstring}')
     print(quantum_circuit)
 
-    return jsonify({'message': "Route successfully ran"}), 200
+    return jsonify({'result': f'{secret_bitstring}'}), 200
